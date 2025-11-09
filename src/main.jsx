bd5843e054e78806bb0cbe544a731fd014b30fc4
hafijurrahmanMILON <hafijurrahman7085@gmail.com>
2025-11-09 15:02:26 +0600

    project initialized

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { router } from "./Router/Routes";
+import AuthProvider from "./Provider/AuthProvider";
+import './index.css'
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+  </StrictMode>
+);
